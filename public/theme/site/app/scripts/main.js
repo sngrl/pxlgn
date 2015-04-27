f2fa84d718e3fcd29e34ddf5ad34348c75ad36b7
Kirill Fadeev <fk@realitygroup.ru>
2015-04-27 15:18:17 +0300

    fix

---
+++ public/theme/site/app/scripts/main.js
@@ -0,0 +1,163 @@
+/* jshint devel:true */
+$('.styled-select').selectmenu().selectmenu( "open" ).selectmenu( "close" );
+
+//Главный слайдер
+
+var mainFotorama = function() {
+	var $fotoramaDiv = $('.main-fotorama').fotorama({
+		width: 620,
+		height: 360,
+		loop: true,
+		arrows: false
+	});
+	var fotorama = $fotoramaDiv.data('fotorama');
+	$('.js-main-left').on('click', function(){
+		fotorama.show('<');
+	});
+	$('.js-main-right').on('click', function(){
+		fotorama.show('>');
+	});
+}
+mainFotorama();
+
+
+//Слайдер видео
+
+var videoFotorama = function() {
+	var $fotoramaDiv = $('.video-fotorama').fotorama({
+		width: 246,
+		height: 184,
+		loop: true,
+		arrows: false
+	});
+	var fotorama = $fotoramaDiv.data('fotorama');
+	$('.js-video-left').on('click', function(){
+		fotorama.show('<');
+	});
+	$('.js-video-right').on('click', function(){
+		fotorama.show('>');
+	});
+}
+videoFotorama();
+
+
+//Слайдер скринов
+
+var screenFotorama = function() {
+	var $fotoramaDiv = $('.screen-fotorama').fotorama({
+		width: 246,
+		height: 184,
+		loop: true,
+		arrows: false,
+		click: false,
+		swipe: false
+	});
+	var fotorama = $fotoramaDiv.data('fotorama');
+	$('.js-screen-left').on('click', function(){
+		fotorama.show('<');
+	});
+	$('.js-screen-right').on('click', function(){
+		fotorama.show('>');
+	});
+}
+screenFotorama();
+
+
+//POP-UP окна авторизиции
+
+$(function() {
+    $( "#check" ).button();
+    $( "#format" ).buttonset();
+
+    $(".button-registration").click(function(e){
+        $(".login-form-background").fadeIn(700);
+        e.preventDefault();
+    });
+
+    $(".close-button").click(function(e){
+        $(".login-form-background").fadeOut(700);
+        e.preventDefault();
+    });
+
+    $(".fancybox").fancybox({})
+
+    // ВАЛИДАЦИЯ
+
+    /*$("form.review-form").validate({
+	   	rules: {
+	     	name: 'required',
+	     	email: {
+	       		required: true,
+	       		email: true
+	     	},
+	     	review: 'required'
+	   	},
+
+	    messages: {
+	     	name: 'Обязательное поле',
+	     	email: {
+	       		required: 'Обязательное поле',
+	       	email: 'Неверный формат. Попробуйте еще'
+	     	},
+	     	review: 'Обязательное поле'
+	   },
+
+	   submitHandler: function(form) {
+	     	var _url = $(form).attr('action'),
+	        	_data = $(form).serialize(),
+	        	_method = $(form).attr('method')||'POST';
+	     	$.ajax({
+	       		type: _method,
+	       		url: _url,
+	       		data: _data,
+		       	success: function(data) {
+		         	$('.unit.form-holder .final').fadeIn();
+		       	}
+	     	})
+	   	}
+ 	});*/
+	
+	// ПОКАЗАТЬ-СКРЫТЬ ПАРОЛЬ
+
+	$('#log-in-form button.spice').click(function(e){
+		e.preventDefault();
+
+		var $input = $('#log-in-form .pass input');
+		var _type = $input.attr('type');
+
+		if (_type == 'password') {
+			$input.attr('type', 'text');
+		} else if (_type == 'text') {
+			$input.attr('type', 'password');
+		}
+		$('form.page button.spice').toggleClass( "spice-pressed" );
+	});
+
+	// РАБОТА ФОРМЫ
+
+	$('.log-in-button-2 button').click(function(e){
+		e.preventDefault();
+
+		$('#log-in-form').toggleClass('log-in-form');
+
+	});
+
+	$('.log-in-button-2 button').click(function(e){
+		e.preventDefault();
+
+		$('.form-fade').hide();
+		$('.success-fade').show();
+	});
+
+	$('.download-button a').click(function(e){
+		e.preventDefault();
+
+		$('.form-fade').show();
+		$('.success-fade').hide();
+	});
+
+	//$('.app-store').
+
+});
+
+
