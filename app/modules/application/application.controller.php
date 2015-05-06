3066971ea61105565d0df487cdf11365e66600d8
sngrl <reserved@mail.ru>
2015-05-06 12:06:21 +0300

    fix

--- app/modules/application/application.controller.php
+++ app/modules/application/application.controller.php
@@ -10,11 +10,15 @@ class ApplicationController extends BaseController {
     ## Routing rules of module
     public static function returnRoutes($prefix = null) {
 
-        Route::group(array(), function() {
+        Route::group(array('prefix' => '{lang}'), function() {
 
+            Route::get('/registration-ok', array('as' => 'app.registration-ok', 'uses' => __CLASS__.'@getRegistrationOk'));
+
+            /*
             Route::any('/ajax/request-call', array('as' => 'ajax.request-call', 'uses' => __CLASS__.'@postRequestCall'));
             Route::any('/ajax/send-message', array('as' => 'ajax.send-message', 'uses' => __CLASS__.'@postSendMessage'));
             Route::any('/ajax/architects-competition', array('as' => 'ajax.architects-competition', 'uses' => __CLASS__.'@postArchitectsCompetition'));
+            */
         });
     }
 
@@ -27,9 +31,11 @@ class ApplicationController extends BaseController {
 	}
 
 
-    public function postRequestCall() {
+    public function getRegistrationOk() {
 
-        #
+        $link = "#";
+        $nickname = "Werewombat";
+        return View::make(Helper::layout('registration-ok'), compact('asd'));
     }
 
 
