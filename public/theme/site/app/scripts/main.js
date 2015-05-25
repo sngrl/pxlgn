f49f13ff2880d9dd840728093f8ec3bd9a5e383a
Kirill Fadeev <fk@realitygroup.ru>
2015-05-25 19:48:28 +0300

    fix

--- public/theme/site/app/scripts/main.js
+++ public/theme/site/app/scripts/main.js
@@ -198,13 +198,13 @@ var mediaMenu = function(){
 	var active_menu_item = menu_item.filter('.active');
 	var menu_height = 60;
 	var setActive = function() {
-		parent.css('background-position', '0 ' + '-' menu_height*active_menu_item.index() + 'px');
+		parent.css('background-position', '0 ' + '-' + menu_height*active_menu_item.index() + 'px');
 	}
 	var init = function() {
 		setActive();
 		menu_item.on('mouseenter', function(){
 			var this_index = $(this).index();
-			parent.css('background-position', '0 ' + '-'  menu_height*this_index + 'px');
+			parent.css('background-position', '0 ' + '-' + menu_height*this_index + 'px');
 		}).on('mouseleave', function(){
 			setActive();
 		});
