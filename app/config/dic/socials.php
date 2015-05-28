57ca361dd50ba72af88a06e0072b83014ba20141
sngrl <reserved@mail.ru>
2015-05-28 17:28:34 +0300

    fix

--- app/config/dic/socials.php
+++ app/config/dic/socials.php
@@ -5,12 +5,12 @@ return array(
     'fields' => function() {
 
         return array(
-            /*
+            #/*
             'image' => array(
                 'title' => 'Изображение',
                 'type' => 'image',
             ),
-            */
+            #*/
             'link' => array(
                 'title' => 'Ссылка',
                 'type' => 'text',
