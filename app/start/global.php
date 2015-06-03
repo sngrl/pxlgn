116b427434b5dd10e54f2bc8e185f89ac7b3a567
sngrl <reserved@mail.ru>
2015-06-03 20:32:25 +0300

    fix

--- app/start/global.php
+++ app/start/global.php
@@ -153,6 +153,12 @@ require app_path().'/filters.php';
     #Helper::d( '/' . Request::segment(1) . '/ - (' . Config::get('app.default_locale') . ') => (' . Config::get('app.locale') . ") - " . Session::get('locale') );
 #*/
 
+##
+## Загружаем настройки из кеша
+##
+$settings = Cache::get('cms.settings');
+Config::set('app.settings', $settings);
+
 #Event::listen('illuminate.query', function($query){ echo $query . "<br/>\n"; });
 #Event::listen('*', function($query){
 #Event::listen('eloquent.booting: *', function($query){
