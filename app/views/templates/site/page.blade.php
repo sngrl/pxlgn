2231ad2e0148962c4cd409a161dcd50f53394172
Kirill Fadeev <fk@realitygroup.ru>
2015-06-03 21:37:47 +0300

    fix

---
+++ app/views/templates/site/page.blade.php
@@ -0,0 +1,31 @@
+<?
+/**
+ * TITLE: FAQ
+ * AVAILABLE_ONLY_IN_ADVANCED_MODE
+ */
+?>
+@extends(Helper::layout())
+
+
+@section('style')
+@stop
+
+
+@section('content')
+
+    <div class="inner-content">
+        <div class="faq-about">
+
+            {{ $page->block('about') }}
+
+        </div>
+
+        {{ $page->block('questions') }}
+
+    </div>
+
+@stop
+
+
+@section('scripts')
+@stop
